5d5f7c2cf07212ac5035121812c32d8de8cfb947
cierraostiguy <122482226+cierraostiguy@users.noreply.github.com>
2023-03-07 11:12:39 -0500

    Add tabs for capitalizatoin

---
+++ src/stories/Capitalization.jsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+// interface TabPanelProps {
+//   children?: React.ReactNode;
+//   index: number;
+//   value: number;
+// }
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Capitalization" {...a11yProps(0)} />
+          <Tab label="Acronyms" {...a11yProps(1)} />
+          <Tab label="Wayfinding" {...a11yProps(2)} />
+          <Tab label="Accessibility Notes" {...a11yProps(3)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Capitalization
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Acronyms
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Wayfinding
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Accessibility Notes
+      </TabPanel>
+    </Box>
+  );
+}
