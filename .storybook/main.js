24a6bb7ba3e01d15806c1036d74fc71cb3e2a994
macbisdesign <costiguy@fearsol.com>
2023-03-12 13:32:00 -0400

    docs deployment

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
   "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "outputDir": "./docs/",
   // "addons": [{
   //   name: "@storybook/addon-docs",
   //   options: {
